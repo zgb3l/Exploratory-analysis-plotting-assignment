3b8aaad805d5f4e61effe90b9ac5e01adcf61dc7
zgb3l <zgb3l@outlook.com>
2021-12-23 23:20:23 +0100

    Adding last plots

---
+++ plot3.R
@@ -0,0 +1,13 @@
+# Creating plot 3
+
+with(subpower, {
+  plot(Sub_metering_1~dateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+
+# Adding and customizing legend
+
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
